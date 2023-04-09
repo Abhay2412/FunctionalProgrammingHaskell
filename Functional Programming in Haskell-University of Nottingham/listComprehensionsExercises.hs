e0c480dd6a4f8498ecf779380e10a788f6dc636e
Abhay2412 <abhaykhosla0@gmail.com>
2023-04-09 11:57:50 -0600

    Learn about string, list comprehensions and zip function

---
+++ Functional Programming in Haskell-University of Nottingham/listComprehensionsExercises.hs
@@ -0,0 +1,13 @@
+-- Generating primes
+factors :: Int -> [Int]
+factors n = [x | x <- [1..n], n `mod` x == 0]
+
+prime :: Int -> Bool
+prime n = factors n == [1, n]
+
+primes :: Int -> [Int]
+primes n = [x | x <- [2..n], prime x]
+
+-- Exercise 2
+perfect :: Int -> Bool
+perfect n = sum(init(factors n)) == n
