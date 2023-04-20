68312b3a69bb3a6b2ab443bc4a652082cc8d7300
Abhay2412 <abhaykhosla0@gmail.com>
2023-04-20 10:02:22 -0600

    Implement types into the functions and adding p, h1 tags

--- blogGenerator/blogGenerate.hs
+++ blogGenerator/blogGenerate.hs
@@ -1,11 +1,30 @@
--- Exercise 1
-html_ content = "<html>" <> content <> "</html>" 
-body_ content = "<body>" <> content <> "</body>"
--- Exercise 2
--- Exercise 3
-head_ content = "<head>" <> content <> "</head>" 
-title_ content = "<title>" <> content <> "</title>"
--- Exercise 4
+-- Chapter 3 - Building an HTML Printer Library
+el :: String -> String -> String
+el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">" 
+
+html_ :: String -> String
+html_ = el "html"
+
+body_ :: String -> String
+body_ = el "body"
+
+head_ :: String -> String
+head_ = el "head" 
+
+title_ :: String -> String
+title_ = el "title"
+
+p_ :: String -> String
+p_ = el "p"
+
+h1_ :: String -> String
+h1_ = el "h1"
+
+makeHtml :: String -> String -> String
 makeHtml titleString bodyContent = html_ (head_ (title_ titleString) <> body_ bodyContent)
-myHtml = makeHtml "My page title" "My page content"
+
+myHtml :: String
+myHtml = makeHtml "My page title" (h1_ "Hello World!" <> p_ "This is testing the new two functions!")
+
+main :: IO()
 main = putStrLn myHtml
