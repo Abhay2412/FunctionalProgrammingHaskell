6c35acdcf5d12329e1338889d1ed4d3e2baf55bf
Abhay2412 <abhaykhosla0@gmail.com>
2023-04-24 18:20:37 -0600

    Finish creating the HTML library in Haskell, also add Internal modules hiding from the user

---
+++ blogGenerator/Html/Internal.hs
@@ -0,0 +1,71 @@
+module Html.Internal where
+
+-- Chapter 3 - Building an HTML Printer Library
+newtype Html = Html String
+
+newtype Structure = Structure String
+
+type Title = String
+
+el :: String -> String -> String
+el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">" 
+
+html_ :: Title -> Structure -> Html
+html_ title content =
+  Html
+    ( el "html"
+      ( el "head" (el "title" (escape title) )
+        <> el "body" (getStructureString content)
+      )
+    )
+
+body_ :: String -> String
+body_ = el "body"
+
+head_ :: String -> String
+head_ = el "head" 
+
+title_ :: String -> String
+title_ = el "title"
+
+p_ :: String -> Structure
+p_ = Structure . el "p" . escape
+
+h1_ :: String -> Structure
+h1_ = Structure . el "h1" . escape
+
+ul_ :: [Structure] -> Structure
+ul_ = Structure . el "ul" . concat . map (el "li" . getStructureString)
+
+ol_ :: [Structure] -> Structure
+ol_ = Structure . el "ol" . concat . map (el "li" . getStructureString)
+
+code_ :: String -> Structure
+code_ = Structure . el "pre" . escape
+
+getStructureString :: Structure -> String
+getStructureString content =
+  case content of
+    Structure str -> str
+
+render :: Html -> String
+render html =
+  case html of
+    Html str -> str
+
+append_ :: Structure -> Structure -> Structure
+append_ c1 c2 = Structure (getStructureString c1 <> getStructureString c2)
+
+escape :: String -> String 
+escape =
+    let
+        escapeChar c =
+            case c of 
+                '<' -> "&lt;"
+                '>' -> "&gt;"
+                '&' -> "&amp;"
+                '"' -> "&quot;"
+                '\''-> "&#39;"
+                _   -> [c]
+    in
+        concat . map escapeChar
